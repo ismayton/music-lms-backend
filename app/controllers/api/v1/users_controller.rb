bbeee7d081240f5cd4c71cb9e9bf5faecda129d1
ismayton <ian.mayton@gmail.com>
2021-04-06 16:06:17 -0500

    User controller Create functionality added

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,5 +4,21 @@ class Api::V1::UsersController < ApplicationController
         render json: users
     end
     
+    def create
+        if User.find_by(username: params[:username])
+            render json: {error: "Username already taken."}
+        else
+            if params[:username]
+                user = User.new(username: params[:username])
+                user.password = params[:password]
+                if user.save
+                    render json: user 
+                end
+            else
+                remder json: {error: "User could not be created."}
+            end
+        end
+    end
+
     # delete index? add show
 end
