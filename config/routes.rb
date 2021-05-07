49f35bc9ee831c93425afb88c91aeecf25d6828d
ismayton <ian.mayton@gmail.com>
2021-05-06 20:31:41 -0500

    route change lesson_statuses to api/v1

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  resources :lesson_statuses
+  resources 
   namespace :api do
     namespace :v1 do
       post '/login',    to: 'sessions#create'
       post '/logout',   to: 'sessions#destroy'
       get '/logged_in', to: 'sessions#is_logged_in?'
       
-      resources :courses, :users, :teachers, :subscriptions
+      resources :courses, :users, :teachers, :subscriptions, :lesson_statuses
     end
   end 
 end
