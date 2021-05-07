226481db6de29c4d0b7b247ff3eb99542fe9567c
ismayton <ian.mayton@gmail.com>
2021-05-06 21:45:01 -0500

    cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,11 +7,10 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'academy.ianmayton.com'
+    origins '*'
 
     resource '*',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
-      credentials: true
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
